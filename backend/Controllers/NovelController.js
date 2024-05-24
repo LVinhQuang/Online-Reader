1bde06df72ab9df3ba286a79a07d678a3ce552b6
21120551 <damtutam@gmail.com>
2024-05-24 16:26:35 +0700

    tam_edit GetFeaturedNovel function name to GetFeaturedNovels

--- backend/Controllers/NovelController.js
+++ backend/Controllers/NovelController.js
@@ -2,7 +2,7 @@ const Domain1 = require('../Services/domain1');
 //TODO: thay Domain1 thành factory
 
 module.exports = {
-    GetFeaturedNovel: async function(req, res){
+    GetFeaturedNovels: async function(req, res){
         try{
             const Domain = new Domain1();//
             let Data = await Domain.GetFeaturedNovel();
@@ -14,4 +14,5 @@ module.exports = {
             res.status(500).json({message: error});
         }
     }
+
 }
