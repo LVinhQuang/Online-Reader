6bf88fc9f03e4864a8bf33cbcd07f3ab53e4eb97
21120538 <luuvinhquangqg@gmail.com>
2024-05-27 20:46:33 +0700

    change headless to true

--- backend/Services/truyenfull.js
+++ backend/Services/truyenfull.js
@@ -6,7 +6,7 @@ module.exports = class TruyenFull {
     }
 
     async GetFeaturedNovels() {
-        const browser = await puppeteer.launch({ headless: false });
+        const browser = await puppeteer.launch({ headless: true });
         const page = await browser.newPage();
         await page.goto(this.baseUrl, { waitUntil: 'networkidle2' });
         const data = await page.evaluate(() => {
